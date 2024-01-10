20b94eafb78c13898ecc2f093c4e0ca56b9e757c
Yassine Zemmouriate <zemmouriat@gmail.com>
2024-01-10 14:34:11 +0100

    :lipstick: Adding SideBar Component

---
+++ src/components/SideBar.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+export interface SideBarProps {
+
+}
+
+const SideBar : React.FC<SideBarProps> = () => {
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default SideBar;
